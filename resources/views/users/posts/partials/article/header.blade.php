a55be3a10c17589e5926a1005e0a6d21c56a58cf
Al Razi Abdul Alim <abdulalimpvt@gmail.com>
2024-10-03 10:59:00 +0600

    Set show icon and link

--- resources/views/users/posts/partials/article/header.blade.php
+++ resources/views/users/posts/partials/article/header.blade.php
@@ -21,15 +21,25 @@
 
         <div class="flex flex-shrink-0 self-center">
             <div class="relative inline-block text-left">
-                <div>
+                <div class="flex items-center space-x-2">
                     <a href="{{ route('posts.edit', ['post' => $post->id]) }}">
                         <button type="button"
                             class="-m-2 flex items-center rounded-full p-2 text-gray-400 hover:text-gray-600"
                             id="menu-0-button">
-                            <span class="sr-only">Open options</span>
+                            <span class="sr-only">Edit</span>
                             <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                 <path
                                     d="M17.414 2.586a2 2 0 00-2.828 0l-9 9A2 2 0 005 13v2.586A1.5 1.5 0 006.5 17H9a2 2 0 001.414-.586l9-9a2 2 0 000-2.828l-2-2zM10 15H6v-1.414l7.586-7.586 1.414 1.414L10 15zm7.586-7.586l-1.414-1.414 1.293-1.293a1 1 0 111.414 1.414l-1.293 1.293z" />
+                            </svg>
+                        </button>
+                    </a>
+                    <a href="{{ route('posts.show', ['post' => $post->id]) }}">
+                        <button type="button"
+                            class="-m-2 flex items-center rounded-full p-2 text-gray-400 hover:text-gray-600"
+                            id="menu-0-button">
+                            <span class="sr-only">Show</span>
+                            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <path d="M2 3a1 1 0 011-1h10a1 1 0 011 1v14a1 1 0 01-1 1H3a1 1 0 01-1-1V3zm2 0v14h10V3H4zm1 1h8v1H5V4zM5 8h8v1H5V8zm0 4h8v1H5v-1z" />
                             </svg>
                         </button>
                     </a>
